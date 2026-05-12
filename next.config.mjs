e95d48d58096b8289b7d53f0cd55d5646131ef98
Insider <insider@mooze.com>
2026-05-12 23:42:05 +0200

    Integrate Production-Grade Javascript Obfuscation Layer

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,31 @@
+import WebpackObfuscator from 'webpack-obfuscator';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.plugins.push(
+        new WebpackObfuscator({
+          rotateStringArray: true,
+          stringArray: true,
+          stringArrayThreshold: 0.75,
+          controlFlowFlattening: true,
+          controlFlowFlatteningThreshold: 0.75,
+          deadCodeInjection: true,
+          deadCodeInjectionThreshold: 0.4,
+          identifierNamesGenerator: 'hexadecimal',
+          renameGlobals: false,
+          selfDefending: true,
+          compact: true,
+          splitStrings: true,
+          splitStringsChunkLength: 10,
+          unicodeEscapeSequence: true
+        }, ['_next/static/chunks/webpack-*.js'])
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
